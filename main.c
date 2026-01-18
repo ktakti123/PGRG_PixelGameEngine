df446c4562a52d7f389520d2c61e0d931894f2b3
Shankar Subedi <ktakti123@gmail.com>
2026-01-18 20:59:32 +0545

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,12 @@ int main()
   {                             // Main application loop
     Events();                   // Handle events (like user inputs)
     fill_screen(255, 255, 255); // Clear screen to white
+    set_point(10,20,255,255,255);// Draw point at x1,y1,x2,y2
+    set_point(10,20,color_black);
+    set_point(100,20,color_red);
+    set_point(10,20,color_blue);
+    set_point(10,20,color_green);
+    set_point(10,20,color_white);
     Render(); // Render everything on screen
   }
   
